64b4776d3e6f72232bedbb29ec77a9f08f08373f
chai53 <skymise@gmail.com>
2022-09-01 09:29:20 +0900

    feat: 7-1 Encapsulate Record

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,38 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
 
-organization.name = 'Dream Coding';
+  constructor(data) {
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(value) {
+    this.#name = value;
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  set country(value) {
+    this.#country = value;
+  }
+
+  get rawData() {
+    return { name: this.name, country: this.country };
+  }
+}
+
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
+
+organization.name = "Dream Coding";
 console.log(organization.name);
 console.log(organization.country);
