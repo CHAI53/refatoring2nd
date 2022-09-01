0b84b17e0eb8360033df3e58190fe749327988f5
chai53 <skymise@gmail.com>
2022-09-02 08:59:04 +0900

    feat: 7-4 Replace Temp with Query

--- ch7/7-4.js
+++ ch7/7-4.js
@@ -6,10 +6,15 @@ class Order {
     this.#item = item;
   }
 
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98;
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 }
