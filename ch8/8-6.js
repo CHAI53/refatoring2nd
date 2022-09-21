d0712e94bec2d24f0ba79ad138c4ffe518336a5d
chai53 <skymise@gmail.com>
2022-09-22 08:55:17 +0900

    feat: 8-6 Slide Statements

--- ch8/8-6.js
+++ ch8/8-6.js
@@ -1,18 +1,16 @@
 // 예제 1
 const pricingPlan = retrievePricingPlan();
+const chargePerUnit = pricingPlan.unit;
 const order = retreiveOrder();
 let charge;
-const chargePerUnit = pricingPlan.unit;
 
 // 예제 2
 function someFunc() {
-  let result;
-  if (availableResources.length === 0) {
-    result = createResource();
-    allocatedResources.push(result);
-  } else {
-    result = availableResources.pop();
-    allocatedResources.push(result);
-  }
+  const result =
+    availableResources.length === 0
+      ? createResource()
+      : availableResources.pop();
+
+  allocatedResources.push(result);
   return result;
 }
