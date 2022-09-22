05dbe68dd211b23ac7e78ddc24a65c95a002d7d7
chai53 <skymise@gmail.com>
2022-09-22 09:01:46 +0900

    feat: 8-7 Split Loop

--- ch8/8-7.js
+++ ch8/8-7.js
@@ -1,10 +1,11 @@
 export function reportYoungestAgeAndTotalSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
+  return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+
+  function youngestAge() {
+    return Math.min(...people.map((p) => p.age));
   }
 
-  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+  function totalSalary() {
+    return people.reduce((total, p) => (total += p.salary), 0);
+  }
 }
