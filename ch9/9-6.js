a1b43c227182a2b57fdb6f9529e4cf227fa148cc
chai53 <skymise@gmail.com>
2022-09-28 08:59:56 +0900

    feat: 9-6 Replace Magic Literal

--- ch9/9-6.js
+++ ch9/9-6.js
@@ -1,3 +1,7 @@
+const STANDARD_GRAVITY = 9.81;
 function potentialEnergy(mass, height) {
-  return mass * 9.81 * height;
+  return mass * STANDARD_GRAVITY * height;
 }
+
+const MAX_ITEMS = 5;
+item * MAX_ITEMS;
