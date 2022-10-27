ce945701706f830db79e25bcbbd32e78f77493c2
chai53 <skymise@gmail.com>
2022-10-27 09:30:34 +0900

    feat: 11-1 Separate Query from Modifier

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,30 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
-  const result = customer.invoices.reduce(
-    (total, each) => each.amount + total,
-    0
-  );
-  sendBill();
-  return result;
+function getTotalOutstanding() {
+  return customer.invoices.reduce((total, each) => each.amount + total, 0);
+}
+
+function sendBill() {
+  // bill을 보냄
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant() {
   for (const p of people) {
-    if (p === 'Don') {
-      setOffAlarms(alarm, p);
-      return 'Don';
+    if (p === "Don") {
+      return "Don";
     }
-    if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
+    if (p === "John") {
+      return "John";
     }
   }
-  return '';
+  return "";
 }
 
 function setOffAlarms(alarm, p) {
-  alarm.setOff('Found Miscreant ' + p);
+  alarm.setOff("Found Miscreant " + p);
 }
