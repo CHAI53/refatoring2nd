0b48d75a24d1eeb2a43ca7e11d55a68bc2da4dd8
chai53 <skymise@gmail.com>
2022-10-27 09:09:00 +0900

    feat: 10-1 Decompose Conditional

---
+++ ch10/10-1.js
@@ -0,0 +1,15 @@
+function calculateCharge(date, quantity, plan) {
+  return isSummer() ? summerCharge() : regularCharge();
+
+  function isSummer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  }
+
+  function summerCharge() {
+    return quantity * plan.summerRate;
+  }
+
+  function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
+}
