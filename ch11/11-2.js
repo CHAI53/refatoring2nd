b6236cfaca3750744fe2ca04b095192e94e9ccaf
chai53 <skymise@gmail.com>
2022-10-27 09:39:01 +0900

    feat: 11-2 Parameterize Function

--- ch11/11-2.js
+++ ch11/11-2.js
@@ -1,36 +1,34 @@
 // 예제 1
-function tenPercentRaise(person) {
-  person.salary = person.salary.multiply(1.1);
-}
+// function tenPercentRaise(person) {
+//   person.salary = person.salary.multiply(1.1);
+// }
 
-function fivePercentRaise(person) {
-  person.salary = person.salary.multiply(1.05);
+// function fivePercentRaise(person) {
+//   person.salary = person.salary.multiply(1.05);
+// }
+
+function salaryRaise(person, factor) {
+  person.salary = person.salary.multiply(factor);
 }
 
 // 예제 2
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    withinBand(usage, 0, 100) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    withinBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
-}
-
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0;
+function withinBand(usage, bottom, top) {
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function usd(value) {
   return {
-    currency: '$',
-    currencyName: 'USD',
+    currency: "$",
+    currencyName: "USD",
     value: value,
   };
 }
