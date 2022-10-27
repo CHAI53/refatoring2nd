c033410044e40f730196801fb95a5c91bd284aa0
chai53 <skymise@gmail.com>
2022-10-27 09:43:45 +0900

    feat: 11-3 Remove Flag Argument

--- ch11/11-3.js
+++ ch11/11-3.js
@@ -1,19 +1,30 @@
 // 예제 1
-function setDimension(name, value) {
-  if (name === 'height') {
-    this._height = value;
-    return;
-  }
-  if (name === 'width') {
-    this._width = value;
-    return;
-  }
+function setWidth(value) {
+  this._width = value;
 }
+
+function setHeight(value) {
+  this._height = value;
+}
+
+// function setDimension(name, value) {
+//   if (name === 'height') {
+//     this._height = value;
+//     return;
+//   }
+//   if (name === 'width') {
+//     this._width = value;
+//     return;
+//   }
+// }
 
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regularBook(customer) {}
+  premiumBook(customer) {}
+  #book(customer, isPremium) {} // 내부에선 이렇게 사용가능. 하지만 API spec으로는 부적절.
 }
 
 // 예제 3
-function setSwitch(on);
+function switchOn();
+function switchOff();
