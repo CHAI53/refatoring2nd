5de6373f66efefc047350ed4d9a2aa960d2d7273
chai53 <skymise@gmail.com>
2022-11-10 09:36:11 +0900

    Feat: 12-1 Pull Up Method

--- ch12/12-1.js
+++ ch12/12-1.js
@@ -1,24 +1,18 @@
 // 예시 1
-class Employee {}
-
-class Salesperson extends Employee {
+class Employee {
   get name() {}
 }
 
-class Engineer extends Employee {
-  get name() {}
-}
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
 
 // 예시 2
-class Party {}
-
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
-  }
-}
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
+
+class Department extends Party {}
+class Employee extends Party {}
