eb89f63fd1fca35dcaab1a364dcfb81c3e8c249a
chai53 <skymise@gmail.com>
2022-11-10 09:13:13 +0900

    Feat: 11-12 Replace Error Code with Exception

--- ch11/11-12.js
+++ ch11/11-12.js
@@ -1,4 +1,18 @@
 function localShippingRules(data) {
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw new OrderProcessingError(-23);
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super();
+    this.errorCode = errorCode;
+  }
+}
+try {
+  const result = localShippingRules();
+} catch (error) {
+  if (error instanceof OrderProcessingError) {
+    console.log(error);
+  }
 }
