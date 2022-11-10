ec90717756d58257f93e0d2c405d65cbc647458e
chai53 <skymise@gmail.com>
2022-11-10 09:13:42 +0900

    Feat: 11-13 Replace Exception with Precheck

--- ch11/11-13.js
+++ ch11/11-13.js
@@ -1,14 +1,10 @@
 const values = [];
 function getValueForPeriod(periodNumber) {
-  const value = values[periodNumber];
-  if (!value) {
-    throw new Error('value is undefined');
-  }
-  return value;
+  return values[periodNumber] ?? 0;
+  // if (periodNumber < 0 || periodNumber >= values.length) {
+  //   return 0;
+  // }
+  // return values[periodNumber];
 }
 
-try {
-  getValueForPeriod(-10);
-} catch (error) {
-  console.log('에러 발생!');
-}
+getValueForPeriod(-10);
