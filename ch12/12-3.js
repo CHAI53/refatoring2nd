99ff290cdbcabb8467bdeef6595393c4018de2f1
chai53 <skymise@gmail.com>
2022-11-10 09:39:48 +0900

    Feat: 12-3 Pull Up Constuctor Body

--- ch12/12-3.js
+++ ch12/12-3.js
@@ -1,26 +1,28 @@
-class Party {}
+class Party {
+  #name;
+  constructor(name) {
+    // name에 대한 공통 로직 처리는 여기서
+    this.#name = name;
+  }
+}
 
 class Employee extends Party {
-  #name;
   #id;
   #monthlyCost;
   constructor(name, id, monthlyCost) {
-    super();
-    this.#name = name;
+    super(name);
     this.#id = id;
     this.#monthlyCost = monthlyCost;
   }
 }
 
 class Department extends Party {
-  #name;
   #staff;
   constructor(name, staff) {
-    super();
-    this.#name = name;
+    super(name);
     this.#staff = staff;
   }
 }
 
-const ellie = new Employee('엘리', 123, 13);
-const department = new Department('개발부서', ellie);
+const ellie = new Employee("엘리", 123, 13);
+const department = new Department("개발부서", ellie);
