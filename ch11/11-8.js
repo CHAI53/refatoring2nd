fe2b1dca269d0419cfd8b6a6845d0b8ba411efa1
chai53 <skymise@gmail.com>
2022-11-10 09:09:39 +0900

    Feat: 11-8 Replace Constructor with Factory Function

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -12,6 +12,19 @@ export class Employee {
   }
 
   static get legalTypeCodes() {
-    return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
+    return { E: "Engineer", M: "Manager", S: "Salesman" };
+  }
+
+  static createEngineer(name) {
+    return new Employee(name, "E");
+  }
+
+  static createSeniorEngineer(name) {
+    return new Employee(name, "SE");
+  }
+  static createMarketer(name) {
+    return new Employee(name, "M");
   }
 }
+
+const employee = Employee.createEngineer("엘리");
