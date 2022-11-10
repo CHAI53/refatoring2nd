35c957b34d5128b0529fbc9f4b7d29d7ea2be154
chai53 <skymise@gmail.com>
2022-11-10 09:10:56 +0900

    Feat: 11-10 Replace Command with Function

--- ch11/11-10.js
+++ ch11/11-10.js
@@ -1,3 +1,8 @@
+export function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
+}
+
 export class ChargeCalculator {
   constructor(customer, usage, provider) {
     this._customer = customer;
